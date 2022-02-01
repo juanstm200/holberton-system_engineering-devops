c94f753734aa0365a9c78ab9fbe56e961deeb9b5
juanstm200 <juans.tovar70@gmail.com>
2022-01-31 22:47:26 -0500

    dictionary_of_list - Api

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''  Python script to export data in the JSON format '''
+''' for an employee ID, gives its information. '''
 
 
 if __name__ == '__main__':
@@ -10,21 +10,21 @@ if __name__ == '__main__':
     employee_id = sys.argv[1]
     base_url = 'https://jsonplaceholder.typicode.com/'
     url_user = base_url + 'users/{}'.format(employee_id)
-    url = base_url + 'todos'
+    url_todo = base_url + 'todos'
     info_user = requests.get(url_user).json()
-    all = requests.get(url).json()
+    all_todo = requests.get(url_todo).json()
 
     username = info_user.get('username')
-    employee = {}
+    employee_json = {}
 
     with open('{}.json'.format(employee_id), 'w+') as file:
         tasks = []
-        for todo in all:
+        for todo in all_todo:
             if todo.get('userId') == int(employee_id):
                 task = {}
                 task['task'] = todo.get('title')
                 task['completed'] = todo.get('completed')
                 task['username'] = username
                 tasks.append(task)
-        employee[employee_id] = tasks
-        file.write(json.dumps(employee))
+        employee_json[employee_id] = tasks
+        file.write(json.dumps(employee_json))
