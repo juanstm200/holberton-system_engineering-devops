3cb43d92831d12ae5e8b07239812e68dd94d60f1
juanstm200 <juans.tovar70@gmail.com>
2022-01-31 22:58:01 -0500

    Json - Api

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -2,29 +2,27 @@
 ''' for an employee ID, gives its information. '''
 
 
-if __name__ == '__main__':
-    import json
-    import sys
-    import requests
+import json
+from urllib import request
 
-    employee_id = sys.argv[1]
-    base_url = 'https://jsonplaceholder.typicode.com/'
-    url_user = base_url + 'users/{}'.format(employee_id)
-    url_todo = base_url + 'todos'
-    info_user = requests.get(url_user).json()
-    all_todo = requests.get(url_todo).json()
 
-    username = info_user.get('username')
-    employee_json = {}
-
-    with open('{}.json'.format(employee_id), 'w+') as file:
-        tasks = []
-        for todo in all_todo:
-            if todo.get('userId') == int(employee_id):
-                task = {}
-                task['task'] = todo.get('title')
-                task['completed'] = todo.get('completed')
-                task['username'] = username
-                tasks.append(task)
-        employee_json[employee_id] = tasks
-        file.write(json.dumps(employee_json))
+if __name__ == "__main__":
+    url_user = "https://jsonplaceholder.typicode.com/users"
+    with request.urlopen(url_user) as response:
+        users = json.loads(response.read().decode())
+        data = {}
+        for user in users:
+            url_task = "https://jsonplaceholder.typicode.com/users/{}/todos"\
+                    .format(user.get("id"))
+            with request.urlopen(url_task) as response:
+                tasks = json.loads(response.read().decode())
+                with open("todo_all_employees.json", "w") as file:
+                    list_task = []
+                    for t in tasks:
+                        list_task.append({
+                            "username": user.get("username"),
+                            "completed": t.get("completed"),
+                            "task": t.get("title")
+                        })
+                    data[user.get("id")] = list_task
+                    json.dump(data, file)
